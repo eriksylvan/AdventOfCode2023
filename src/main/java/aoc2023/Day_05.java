9ff63c64408fec14f7a41986ab0de02ece23bc02
Erik Sylvan <erik.sylvan@gmail.com>
2023-12-05 23:39:56 +0100

    Day 05 part two WIP

--- src/main/java/aoc2023/Day_05.java
+++ src/main/java/aoc2023/Day_05.java
@@ -2,8 +2,9 @@ package aoc2023;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.Collections;
-import java.util.stream.LongStream; 
+import java.util.stream.LongStream;
 import java.time.Instant;
 
 /**
@@ -26,22 +27,20 @@ public class Day_05 {
         return inp;
     }
 
-    
-     public long day05PartOne(ArrayList<String> almanack) {
+    public long day05PartOne(ArrayList<String> almanack) {
         int sum = 0;
 
         ArrayList<Long> seeds = new ArrayList<>();
-        
-        ArrayList<ArrayList<Long[]>> maps = new ArrayList<>();
 
+        ArrayList<ArrayList<Long[]>> maps = new ArrayList<>();
 
         String[] seedstr = almanack.get(0).split(": ")[1].trim().split("\s+");
         for (String s : seedstr) {
             seeds.add(Long.parseLong(s));
         }
-        
+
         int mapIndex = -1;
-        //print out seeds
+        // print out seeds
         System.out.println(seeds);
         // loop through the rest of the almanack
         for (int i = 1; i < almanack.size(); i++) {
@@ -51,55 +50,52 @@ public class Day_05 {
             String[] parts = almanack.get(i).split(" ");
             // if part[] is "map:" then continue
             if (parts[1].equals("map:")) {
-                mapIndex ++;
+                mapIndex++;
                 maps.add(new ArrayList<>());
                 continue;
             }
 
-            
             long[] dataRow = new long[3];
             for (int j = 0; j < 3; j++) {
                 dataRow[j] = Long.parseLong(parts[j]);
             }
-            maps.get(mapIndex).add(new Long[]{dataRow[0], dataRow[1], dataRow[2]});
+            maps.get(mapIndex).add(new Long[] { dataRow[0], dataRow[1], dataRow[2] });
         }
-       
 
-        //System.out.println(maps);
+        // System.out.println(maps);
         // for each map
-        
+
         /*
-        for (int i = 0; i < maps.size(); i++) {
-            for (int j = 0; j < maps.get(i).size(); j++) {
-                for (int st : maps.get(i).get(j)) {
-                    System.out.println("map: " + i + " row: " + j + " seed: " + st);
-                }
-            }
-        }
+         * for (int i = 0; i < maps.size(); i++) {
+         * for (int j = 0; j < maps.get(i).size(); j++) {
+         * for (int st : maps.get(i).get(j)) {
+         * System.out.println("map: " + i + " row: " + j + " seed: " + st);
+         * }
+         * }
+         * }
          */
 
         ArrayList<Long> mappedValues = new ArrayList<>();
         // for all seeds
         for (long s : seeds) {
-            //System.out.println("NEW SEED");
+            // System.out.println("NEW SEED");
             long newValue = s;
             // for each map
             for (int i = 0; i < maps.size(); i++) {
                 // for each row in map
-                //System.out.println("NEW MAP");
+                // System.out.println("NEW MAP");
                 newValue = s;
                 for (int j = 0; j < maps.get(i).size(); j++) {
                     // if seed is in row
                     long tmp = mapValue(maps.get(i).get(j)[0], maps.get(i).get(j)[1], maps.get(i).get(j)[2], s);
                     if (tmp == -1) {
                         continue;
-                    }
-                    else {
+                    } else {
                         newValue = tmp;
                         break;
                     }
                 }
-                //System.out.println("new value: " + newValue);
+                // System.out.println("new value: " + newValue);
                 s = newValue;
             }
 
@@ -115,29 +111,26 @@ public class Day_05 {
 
         if (value >= src && value <= src + len) {
             mappedValue = dest + (value - src);
-        } 
+        }
         return mappedValue;
     }
 
-
-
     // Part two
 
-
     public long day05PartTwo(ArrayList<String> almanack) {
-           
-        ArrayList<Long> seeds = new ArrayList<>();
-        
-        ArrayList<ArrayList<Long[]>> maps = new ArrayList<>();
 
+        AtomicLong lowest = new AtomicLong(Long.MAX_VALUE);
+        ArrayList<Long> seeds = new ArrayList<>();
+
+        ArrayList<ArrayList<Long[]>> maps = new ArrayList<>();
 
         String[] seedstr = almanack.get(0).split(": ")[1].trim().split("\s+");
         for (String s : seedstr) {
             seeds.add(Long.parseLong(s));
         }
-        
+
         int mapIndex = -1;
-        //print out seeds
+        // print out seeds
         System.out.println(seeds);
         // loop through the rest of the almanack
         for (int i = 1; i < almanack.size(); i++) {
@@ -147,76 +140,75 @@ public class Day_05 {
             String[] parts = almanack.get(i).split(" ");
             // if part[] is "map:" then continue
             if (parts[1].equals("map:")) {
-                mapIndex ++;
+                mapIndex++;
                 maps.add(new ArrayList<>());
                 continue;
             }
 
-            
             long[] dataRow = new long[3];
             for (int j = 0; j < 3; j++) {
                 dataRow[j] = Long.parseLong(parts[j]);
             }
-            maps.get(mapIndex).add(new Long[]{dataRow[0], dataRow[1], dataRow[2]});
+            maps.get(mapIndex).add(new Long[] { dataRow[0], dataRow[1], dataRow[2] });
         }
-       
 
-        //System.out.println(maps);
+        // System.out.println(maps);
         // for each map
-        
+
         /*
-        for (int i = 0; i < maps.size(); i++) {
-            for (int j = 0; j < maps.get(i).size(); j++) {
-                for (int st : maps.get(i).get(j)) {
-                    System.out.println("map: " + i + " row: " + j + " seed: " + st);
-                }
-            }
-        }
+         * for (int i = 0; i < maps.size(); i++) {
+         * for (int j = 0; j < maps.get(i).size(); j++) {
+         * for (int st : maps.get(i).get(j)) {
+         * System.out.println("map: " + i + " row: " + j + " seed: " + st);
+         * }
+         * }
+         * }
          */
 
         ArrayList<Long> mappedValues = new ArrayList<>();
         // for all seeds in range
-        for (int a=0;a< seeds.size(); a+=2){
-            // Creating an LongStream 
-            System.out.println("a: " + a + " : " + seeds.get(a) + " : " + seeds.get(a+1)+ " : " + Instant.now());
+        for (int a = 0; a < seeds.size(); a += 2) {
+            // Creating an LongStream
+            System.out.println("a: " + a + " : " + seeds.get(a) + " : " + seeds.get(a + 1) + " : " + Instant.now());
 
-            LongStream seedrange = LongStream.range(seeds.get(a), seeds.get(a)+seeds.get(a+1));
+            LongStream seedrange = LongStream.range(seeds.get(a), seeds.get(a) + seeds.get(a + 1) + 1);
 
-        
-                seedrange.forEach(s -> {
-                    //System.out.println("NEW SEED");
-                    long newValue = s;
-                    // for each map
-                    for (int i = 0; i < maps.size(); i++) {
-                        // for each row in map
-                        //System.out.println("NEW MAP");
-                        newValue = s;
-                        for (int j = 0; j < maps.get(i).size(); j++) {
-                            // if seed is in row
-                            long tmp = mapValue(maps.get(i).get(j)[0], maps.get(i).get(j)[1], maps.get(i).get(j)[2], s);
-                            if (tmp == -1) {
-                                continue;
-                            }
-                            else {
-                                newValue = tmp;
-                                break;
-                            }
+            seedrange.forEach(s -> {
+                // System.out.println("NEW SEED");
+                long newValue = s;
+                long startSeed = s;
+
+                // for each map
+                for (int i = 0; i < maps.size(); i++) {
+                    // for each row in map
+                    // System.out.println("NEW MAP");
+                    newValue = s;
+                    for (int j = 0; j < maps.get(i).size(); j++) {
+                        // if seed is in row
+                        long tmp = mapValue(maps.get(i).get(j)[0], maps.get(i).get(j)[1], maps.get(i).get(j)[2], s);
+                        if (tmp == -1) {
+                            continue;
+                        } else {
+                            newValue = tmp;
+                            break;
                         }
-                        //System.out.println("new value: " + newValue);
-                        s = newValue;
                     }
+                    // System.out.println("new value: " + newValue);
+                    s = newValue;
+                }
 
-                    mappedValues.add(newValue);
-                });
-            }
-        
+                // mappedValues.add(newValue);
+                if (newValue < lowest.get()) {
+                    lowest.set(newValue);
+                    System.out.println("new lowest: " + newValue + " startSeed : " + startSeed + " : " + Instant.now());
+                }
+            });
+        }
 
-        Collections.sort(mappedValues);
-        return mappedValues.get(0);
+        // Collections.sort(mappedValues);
+        // return mappedValues.get(0);
+        return lowest.get();
     }
-
-
-
 
     public static void main(String[] args) {
 
@@ -235,8 +227,11 @@ public class Day_05 {
 }
 
 /*
-Advent of code 2023, Day 05
-
-Solution Part one: 600279879
-Solution Part two: 
-*/
+ * Advent of code 2023, Day 05
+ * 
+ * Solution Part one: 600279879
+ * Solution Part two:
+ * WRONG:
+ * Solution Part two: 20191103
+ * 
+ */
